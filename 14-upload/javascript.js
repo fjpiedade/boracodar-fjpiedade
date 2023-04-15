4e2e63bc604a59a48ee77ab181773fc3ee0ec2dd
fjpiedade <fernandp.joao@gmail.com>
2023-04-15 02:32:55 +0100

    add boracodar 14

---
+++ 14-upload/javascript.js
@@ -0,0 +1,7 @@
+const dropArea = document.querySelector('.drag-files')
+dropArea.addEventListener('dragover', ()=>{
+    dropArea.classList.add('dragover')
+})
+dropArea.addEventListener('dragleave', ()=>{
+    dropArea.classList.remove('dragover')
+})
